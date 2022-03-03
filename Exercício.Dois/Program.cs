375153697444e87ac31086c93a2e73d2fe43feb8
Mateus Miranda <>
2022-03-02 21:17:48 -0300

    projeto

---
+++ Exercício.Dois/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercício.Dois
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o valor em USD: ");
+            string valorDigitado = Console.ReadLine();
+            decimal valorConvertido = 0.0m;
+            decimal valorCambio = 5.22m;
+
+            decimal.TryParse(valorDigitado, out valorConvertido);
+
+            decimal valorCambioConvertido = valorConvertido * valorCambio;
+
+            Console.WriteLine($"Valor em $: {valorCambioConvertido} ");
+            Console.ReadLine();
+        }
+    }
+}
